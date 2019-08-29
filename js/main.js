2e8c9272d10ed0a73f62c8b2b18a96d9bf8b77ac
Hazmat88_Desk <matthieu.vinet.hz@gmail.com>
2019-08-29 16:50:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+//import Swup from 'swup';
+
+//const swup = new Swup(); // only this line when included with script tag
+
+  navbar();
+
+
+
+
+
+function navbar(){
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+}
