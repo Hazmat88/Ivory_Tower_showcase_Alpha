e99601cf89544f4ed1a4b7d10d8d49abb70e8341
Hazmat88_Desk <matthieu.vinet.hz@gmail.com>
2019-08-30 19:47:20 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -24,3 +24,32 @@ function navbar(){
   }
 
 }
+
+
+function HeightBackground() {
+      height= $(window).height();
+      $("#cover").css({
+         'height': height
+      })
+
+}
+/*
+function Heightwidth() {
+      width= $(window).width();
+      $("#cover").css({
+         'width': width
+      })
+
+}*/
+
+$(document).ready(function(){
+    HeightBackground();
+    //Heightwidth();
+    $(window).resize(function(){
+        HeightBackground();
+    })
+  /*  $(window).resize(function(){
+        Heightwidth();
+    })*/
+
+})
